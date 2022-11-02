e0497c68d2fc636840e91c77ae443019fcbc1734
Matheus Souza <matheus.souza@psvsistemas.com.br>
2022-11-02 13:17:04 -0300

    setting up EntriesModule

---
+++ src/app/pages/entries/shared/entry.service.ts
@@ -0,0 +1,69 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Entry } from './entry.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EntryService {
+
+  private apiPath = 'api/entries';
+
+  constructor(private _http: HttpClient) { }
+
+  getAll(): Observable<Entry[]> {
+    return this._http.get<Entry[]>(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntries)
+    );
+  }
+
+  getById(id: number): Observable<Entry> {
+    const url = `${this.apiPath}/${id}`
+    return this._http.get<Entry>(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntry)
+    );
+  }
+
+  create(entry: Entry): Observable<Entry> {
+    return this._http.post<Entry>(this.apiPath, entry).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntry)
+    );
+  }
+
+  update(entry: Entry): Observable<Entry> {
+    const url = `${this.apiPath}/${entry.id}`
+    return this._http.put<Entry>(url, entry).pipe(
+      catchError(this.handleError),
+      map(() => entry)
+    );
+  }
+
+  delete(id: number): Observable<void> {
+    const url = `${this.apiPath}/${id}`
+    return this._http.delete(url).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // #region Private METHODS
+  private jsonDataToEntries(jsonData: any[]): Entry[] {
+    const entries: Entry[] = [];
+    jsonData.forEach(entry => entries.push(entry as Entry));
+    return entries;
+  }
+
+  private jsonDataToEntry(jsonData: any): Entry {
+    return jsonData as Entry;
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.log('Erro na requisição => ', error);
+    return throwError(() => error);
+  }
+  // #endregion
+}
