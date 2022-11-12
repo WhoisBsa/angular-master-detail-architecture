bf3aaca79bc4ddeb595fadfa164b2a1ab35a1e66
Matheus Souza <matheus.souza@psvsistemas.com.br>
2022-11-12 11:42:14 -0300

    project improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDataBase } from './in-memory-database';
-import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
