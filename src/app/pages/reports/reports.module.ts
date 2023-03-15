99ed15ffa07591d4835ab9bb33eb993d80d9e753
Matheus Souza <matheus.souza@psvsistemas.com.br>
2023-03-15 15:07:37 -0300

    ft: add reports module and component

---
+++ src/app/pages/reports/reports.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+import { ReportsRoutingModule } from './reports-routing.module';
+import { ReportsComponent } from './reports.component';
+import { SharedModule } from 'primeng/api';
+
+
+@NgModule({
+  declarations: [
+    ReportsComponent
+  ],
+  imports: [
+    ReportsRoutingModule,
+    SharedModule,
+  ]
+})
+export class ReportsModule { }
